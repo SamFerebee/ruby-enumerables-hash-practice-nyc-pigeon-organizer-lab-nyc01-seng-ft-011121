0322de6a67dabb5bbd0247e7d0e35ee73d95d695
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 15:53:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,6 @@ def nyc_pigeon_organizer(data)
   results = data.each_with_object({}) do |(key, value), the_hash|
     value.each do |inner, names|
       names.each do |name|
-        
+        if the_hash!
 
 end
